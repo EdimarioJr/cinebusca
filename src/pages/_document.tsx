33e36d8b4ae8557fb6bca98744b2d90f718d087a
EdimarioJr <edimariojunior.14@gmail.com>
2023-10-15 15:14:28 -0300

    fix: hack to prevent FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script>0</script>
+      </Head>
       <body>
         <Main />
         <NextScript />
