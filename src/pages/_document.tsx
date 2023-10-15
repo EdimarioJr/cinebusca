de6cf0fc68c8e46e0ffc01910bbbb47426fb4feb
EdimarioJr <edimariojunior.14@gmail.com>
2023-10-15 15:54:05 -0300

    fix: FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script>0</script>
-      </Head>
+      <Head />
+
       <body>
+        <script>0</script>
         <Main />
         <NextScript />
       </body>
